79291ba305095ddb7208831b1ef91be1f29f3420
Jendbim <110216507+Jendbim@users.noreply.github.com>
2022-10-28 10:58:25 +0700

    hapus footer admin

--- admin/preparation.php
+++ admin/preparation.php
@@ -324,15 +324,6 @@ require '../cek.php';
                 </div>
                 </div>
             </div>      
-           <!-- Footer -->
-           <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>MIRORIM</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
 
         </div>
         <!-- End of Content Wrapper -->
